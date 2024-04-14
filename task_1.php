4fd90945623f0acbfdea8159812622cc28fab1c5
JuliaPavlicheva <61801735+JuliaPavlicheva@users.noreply.github.com>
2024-04-14 19:50:48 +0300

    ДЗ 5, 6.

--- task_1.php
+++ task_1.php
@@ -1,36 +1,41 @@
 <?php
+declare(strict_types=1);
 
-$bool_true = true;
-$bool_false = false;
-$int = 9;
-$int_zero = 0;
-$float = 1.1;
-$str_int = "9";
-$str_float = "1.1";
-$str_null = "null";
-$str_true = "true";
-$str_false = "false";
-$null = null;
+function show_message(int|float $message): void
+{
+    echo $message . PHP_EOL;
+}
 
-var_dump($bool_true === $str_true); // false
-var_dump($bool_true == $str_true); // true
-var_dump($bool_true == $int); // true
-var_dump($bool_false == $int_zero); // true
-var_dump($bool_false === boolval($int_zero)); // true
-var_dump($int_zero === $null); // false
-var_dump($int_zero == $null); // true
-var_dump($bool_false === $null); // false
-var_dump($str_int == $int); // true
-var_dump($bool_false == $str_false); // false
-var_dump($bool_false == boolval($str_false)); // false
-var_dump($float == $str_float); // true
-var_dump($str_null === $null); // false
-var_dump($float === floatval($str_float)); // true
-var_dump($null == intval($null)); // true
-var_dump(boolval($str_int) === $bool_true); // true
-var_dump(strval($bool_false) == boolval($int_zero)); // true
+function power1(int|float $number, int $power_number = 2): int|float
+{
+    return $number ** $power_number;
+}
 
+$result_power1 = power1(2, 3);
+show_message($result_power1);
 
+function power2(int|float &$number, int $power_number = 2): void
+{
+    $number = $number ** $power_number;
+}
+
+$result_power2 = 9;
+power2($result_power2);
+show_message($result_power2);
+
+function calculate_square1(int|float $radius): int|float
+{
+    return pi() * $radius ** 2;
+}
+show_message(calculate_square1(7));
+
+function calculate_square2(int|float &$radius): void
+{
+    $radius = pi() * $radius ** 2;
+}
+$radius2 = 2;
+calculate_square2($radius2);
+show_message($radius2);
 
 
 
--- task_2.php
+++ task_2.php
@@ -1,59 +1,21 @@
 <?php
+declare(strict_types=1);
 
-echo "Hello! Please, enter a random number\n";
-$value = intval(trim(fgets(STDIN)));
-//$value = 5;
-
-// if / else
-if ($value === 1) {
-    $result = ("green\n");
-} elseif ($value === 2) {
-    $result = ("red\n");
-} elseif ($value === 3) {
-    $result = ("blue\n");
-} elseif ($value === 4) {
-    $result = ("brown\n");
-} elseif ($value === 5) {
-    $result = ("violet\n");
-} elseif ($value === 6) {
-    $result = ("black\n");
-} else {
-    $result = ("white\n");
+function multiply(int $a, int $b, ?closure $callback = null): mixed
+{
+    $result = $a * $b;
+    if(null !== $callback) {
+        return $callback($result);
+    }
+    return $result;
 }
 
-// switch
-switch ($value) {
-    case 1:
-        $result = "green\n";
-        break;
-    case 2:
-        $result = "red\n";
-        break;
-    case 3:
-        $result = "blue\n";
-        break;
-    case 4:
-        $result = "brown\n";
-        break;
-    case 5:
-        $result = "violet\n";
-        break;
-    case 6:
-        $result = "black\n";
-        break;
-    default:
-        $result = "white\n";
-}
+//$multiply_result = multiply(9, 3);
+//echo $multiply_result . PHP_EOL;
 
-// match
-$result = match ($value) {
-    1 => "green\n",
-    2 => "red\n",
-    3 => "blue\n",
-    4 => "brown\n",
-    5 => "violet\n",
-    6 => "black\n",
-    default => "white\n",
-};
+multiply(2, 3, function (int $param_callback): void
+{
+    echo "Result of callback is " . $param_callback . PHP_EOL;
+});
 
-echo $result;
+
